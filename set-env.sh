5735065ff93a56b173e31cd87b3585dc3a5afde4
Pavan Aleti <pavan.hpsa@gmail.com>
2020-02-11 21:54:44 +0000

    setup env

---
+++ set-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Setting environment variables for Terraform"
+export ARM_SUBSCRIPTION_ID=026d3c58-e4a3-4f9b-9b50-45c312cb7ffc
+export ARM_CLIENT_ID=1f3f27ef-c46c-44db-8073-a7f801cf203b
+export ARM_CLIENT_SECRET=0f330662-7341-48fd-a98c-81851e6a587f
+export ARM_TENANT_ID=58abba60-2856-42d0-badb-3309468a76f4
+echo "ENVIRONMENT IS SET"
