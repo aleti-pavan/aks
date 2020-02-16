fce8498344b7248e7e70e085757391cd70fa7d43
Pavan Aleti <pavan.hpsa@gmail.com>
2020-02-16 20:02:46 +0000

    aks cluster vars

---
+++ akscluster/variables.tf
@@ -0,0 +1,12 @@
+variable "location" {
+    type = "string"
+    default = "ukwest"
+}
+variable "rg-name" {
+    type = "string"
+    default = "rg1"
+}
+variable "clustername" {
+    type = "string"
+    default = "aks2aad"
+}
